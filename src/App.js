1ced0f70f0e1277c1076864ab2640406221bfbfa
Shane Mckenna <ShaneMckenna23@hotmail.co.uk>
2017-08-14 22:47:01 +0100

    Added react-motion
    Learning some cool animations!!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from 'react'
 import {Col, Row, Button, Glyphicon, Panel, ListGroupItem, ListGroup, FormControl } from 'react-bootstrap'
 import employeeList from './employeeList.json';
+import {TransitionMotion, spring, presets} from 'react-motion';
+
 
 const filterEmployee = (searchText, maxResults) => {
   return employeeList.filter((employee) => {
-    if (employee.name.toLowerCase().includes(searchText.toLowerCase())) {
+    if (employee.data.name.toLowerCase().includes(searchText.toLowerCase())) {
       return true;
     }
     return false;
   }).slice(0, maxResults);
 }
 
-var maxResults = 5;
+var maxResults = 3;
 
 export default class App extends React.Component {
 
     constructor(){
         super();
         this.state = {
-            selectedEmployee: employeeList[0],
+            selectedEmployee: employeeList[0].data,
             filteredEmployee: filterEmployee('', maxResults)
         }
     }
@@ -84,7 +86,7 @@ class EmployeePanel extends React.Component {
         const {contact} = this.props.employee
 
         return (
-            <Panel header={employeePanelHeader}>
+            <Panel bsClass="employee-card" header={employeePanelHeader}>
                 <ListGroup>
                     <ListGroupItem>{contact.office}</ListGroupItem>
                     <ListGroupItem>{contact.mobile}</ListGroupItem>
@@ -113,11 +115,54 @@ class EmployeePanelHeader extends React.Component {
 
 class EmployeeList extends React.Component {
 
+    getDefaultStyles = () => {
+        let test =  this.props.employeeData.map(employee => ({...employee, style: {height: 0, opacity: 1}}));
+    };
+
+    getStyles = () => {
+    const {employeeData} = this.props;
+    
+    return employeeData.map((employee, i) => {
+      return {
+        ...employee,
+        style: {
+            height: spring(125, presets.gentle),
+            opacity: spring(1, presets.gentle),
+        }
+      };
+    });
+  };
+
+    willEnter() {
+        return {
+            height: 0,
+            opacity: 1,
+        };
+    };
+
+    willLeave() {
+        return {
+            height: spring(0),
+            opacity: spring(0),
+        };
+    };
+
     render() {
         return (
-            <ListGroup>
-                {this.props.employeeData.map(employee => (<EmployeeListItem onEmployeeClick={this.props.onEmployeeClick} employee={employee}/>))}
-            </ListGroup>
+            <TransitionMotion
+            defaultStyles={this.getDefaultStyles()}
+            styles={this.getStyles()}
+            willLeave={this.willLeave}
+            willEnter={this.willEnter}>
+            {styles =>
+               <ListGroup>
+                {styles.map(({key, style, data}) =>
+                  <EmployeeListItem key={key} style={style} employee={data} onEmployeeClick={this.props.onEmployeeClick}/>
+                )}
+                </ListGroup> 
+            }
+          </TransitionMotion>
+            
         );
     }
 }
@@ -125,14 +170,14 @@ class EmployeeList extends React.Component {
 class EmployeeListItem extends React.Component {
 
     onClick = () => {
-        this.props.onEmployeeClick(this.props.employee)
+        this.props.onEmployeeClick(this.props.employee)       
     }
 
     render() {
         const {name, info} = this.props.employee
         const image = "images/" + name + ".jpg"
         return (
-            <div className="employee-item" onClick={this.onClick}>
+            <div className="employee-item" style={this.props.style} onClick={this.onClick}>
                 <img src={image}/>
                 <div className="employee-item-text">
                     <h2>{name}</h2>
